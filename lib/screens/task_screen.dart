59841be960e78fdff57e1fe01bb92ed0c4a52876
Rawnak Chourasia <rnkchr0@gmail.com>
2023-07-08 22:12:33 +0530

    UI Design

---
+++ lib/screens/task_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              padding: const EdgeInsets.only(top: 60, left: 30, bottom: 30),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 30,
+                      child: Icon(
+                        Icons.list,
+                        size: 30,
+                        color: Colors.lightBlueAccent,
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Todoey',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  Text(
+                    '12 tasks',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              )),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
