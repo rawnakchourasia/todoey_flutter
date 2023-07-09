9b6113e646dbc440192ceeb31e7758e9f26d5b6b
Rawnak Chourasia <rnkchr0@gmail.com>
2023-07-09 17:05:36 +0530

    Managing state for checkbox

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,18 +1,54 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     super.key,
   });
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('This is a list'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+      title: Text(
+        'This is a list',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckBox(
+        checkBoxState: isChecked,
+        toggleCheckboxState: (bool? checkboxState) {
+          if (checkboxState != null) {
+            setState(() {
+              isChecked = checkboxState;
+            });
+          }
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  const TaskCheckBox({
+    super.key,
+    required this.checkBoxState,
+    required this.toggleCheckboxState,
+  });
+
+  final bool checkBoxState;
+  final void Function(bool) toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState,
+      onChanged: toggleCheckboxState as void Function(bool?)?,
     );
   }
 }
